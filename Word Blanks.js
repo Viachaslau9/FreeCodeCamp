df21531461129dbd8dc242e68fd948a30a69f127
Viachaslau9 <s3537887@gmail.com>
2022-05-20 12:59:01 +0300

    Word Blanks

---
+++ Word Blanks.js
@@ -0,0 +1,10 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My " + myNoun + " is a " + myAdjective + " and fast " + myVerb + " react " + myAdverb; // Change this line
+// Only change code above this line
+
+//
