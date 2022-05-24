02cc40f2352af2dc5444327440c4405f7471e32d
Viachaslau9 <s3537887@gmail.com>
2022-05-24 13:29:20 +0300

    Accessing Nested Arrays

---
+++ Accessing Nested Objects.js
@@ -0,0 +1,14 @@
+const myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+var gloveBoxContents = myStorage.car.inside["glove box"];
+
+//const gloveBoxContents = undefined;
