726a3477be4136db3d9fbb2155e589d832856760
Viachaslau9 <s3537887@gmail.com>
2022-05-27 13:05:30 +0300

    Override Styles in Subsequent CSS

---
+++ Prioritize One Style Over Another.js
@@ -0,0 +1,11 @@
+<style>
+    body {
+    background - color: black;
+    font-family: monospace;
+    color: green;
+}
+    .pink-text {
+    color: pink;
+}
+</style>
+<h1 className="pink-text">Hello World!</h1>
